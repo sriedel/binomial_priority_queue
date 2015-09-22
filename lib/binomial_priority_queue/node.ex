e91b13201f6d326cd0b920c0a08b991f61ff1ea0
Sven Riedel <sr@gimp.org>
2015-09-22 09:18:06 +0200

    Add node alias for more concise code

--- lib/binomial_priority_queue/node.ex
+++ lib/binomial_priority_queue/node.ex
@@ -1,15 +1,17 @@
 defmodule BinomialPriorityQueue.Node do
   defstruct value: nil, score: nil, size: 1, children: []
 
+  alias BinomialPriorityQueue.Node, as: BPQNode
+
   def new( value, score ) when is_number( score ) do
-    %BinomialPriorityQueue.Node{ value: value, score: score }
+    %BPQNode{ value: value, score: score }
   end
 
   defp new( value, score, size, children ) do
-    %BinomialPriorityQueue.Node{ value: value, score: score, size: size, children: children }
+    %BPQNode{ value: value, score: score, size: size, children: children }
   end
 
-  def merge( a = %BinomialPriorityQueue.Node{ score: score_a }, b = %BinomialPriorityQueue.Node{ score: score_b } ) when score_a <= score_b do
+  def merge( a = %BPQNode{ score: score_a }, b = %BPQNode{ score: score_b } ) when score_a <= score_b do
     new( a.value, a.score, a.size + b.size, [ b | a.children ] )
   end
   def merge( a, b ), do: merge( b, a )
