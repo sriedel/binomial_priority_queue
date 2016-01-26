f0d8096ec74869e21da5b822a60ddffe5032b7ce
Sven Riedel <sr@gimp.org>
2016-01-26 22:20:49 +0100

    Add docs and some spec statements

--- lib/binomial_priority_queue.ex
+++ lib/binomial_priority_queue.ex
@@ -1,24 +1,70 @@
 defmodule BinomialPriorityQueue do
   use GenServer
 
+  @moduledoc """
+  This module implements a priority queue using min-binomial heaps with a
+  GenServer interface. Each value within the priority queue needs to have a
+  score assigned, according to which its position within the queue is computed.
+  
+  This lends itself naturally to use unix timestamps as scores, allowing this
+  module to act as a simple time-based queueing server.
+
+  Note that traditional queuing server functionality such as persistence or
+  protection against lost jobs when an exception is raised after a job is 
+  removed from the queue are not part of the provided functionality as of 
+  the time of this writing.
+
+  For more information on binomial heaps, see 
+  https://en.wikipedia.org/wiki/Binomial_heap
+  """
+
   alias BinomialPriorityQueue.Heap, as: BPQHeap
 
+  @doc """
+  Starts the GenServer with the options passed.
+  Returns `{ :ok, pid }` on success.
+  """
   def start_link( opts \\ [] ) do
     GenServer.start_link( __MODULE__, :ok, opts )
   end
 
+  @doc """
+  Add the _value_ with score _score_ to the server provided by _server_.
+  """
   def add( server, value, score ) when is_number( score ) do
     GenServer.call( server, { :add, value, score } )
   end
 
+  @doc """
+  Returns the value with the minimum score within the queue provided by
+  _server_.  The value is not removed from the queue. 
+
+  Returns _nil_ if the queue is empty.
+
+  """
   def min( server ), do: GenServer.call( server, { :min } )
 
+  @doc """
+  Remove the minimum value from the queue provided by _server_.
+
+  Returns `:empty`, when the queue is already empty, otherwise `:ok`.
+  """
   def pop( server ), do: GenServer.call( server, { :pop } )
 
+  @doc """
+  Removes and returns the minimum value from the queue provided by _server_.
+  Returns nil if the queue is empty.
+  """
   def min_pop( server ), do: GenServer.call( server, { :min_pop } )
 
+  @doc """
+  Returns the number of elements within the queue provided by _server_.
+  """
   def size( server ), do: GenServer.call( server, { :size } )
 
+  @doc """
+  Stops the GenServer identified by _server_.
+  """
   def stop( server ), do: GenServer.call( server, { :stop } )
 
   def init( :ok ) do
--- lib/binomial_priority_queue/heap.ex
+++ lib/binomial_priority_queue/heap.ex
@@ -4,12 +4,25 @@ defmodule BinomialPriorityQueue.Heap do
   alias BinomialPriorityQueue.Node, as: BPQNode
   alias BinomialPriorityQueue.Heap, as: BPQHeap
 
+  @moduledoc """
+  This module can be used to use a queue directly, without a GenServer.
+  """
+
+  @doc """
+  Returns a new Heap structure.
+  """
+  @spec new() :: struct
   def new do
     %BPQHeap{}
   end
   
   defp new( size, forrest ), do: %BPQHeap{ size: size, forrest: forrest }
 
+  @doc """
+  Add the _value_ with score _score_ to the queue identified by _root_.
+  Returns a new heap containing the old heap and the added value.
+  """
+  @spec add( struct, term, number ) :: struct
   def add( root, value, score ) when is_number( score ) do
     node = BPQNode.new( value, score )
 
@@ -25,12 +38,26 @@ defmodule BinomialPriorityQueue.Heap do
       h.size > node.size  -> add_node( [ node | acc ], t, h )
     end
   end
+  
+  @doc """
+  Returns the minimum value of the queue identified by _root_. The value is 
+  not removed from the heap structure. 
 
+  If the queue is empty, returns _nil_.
+  """
+  @spec min( struct ) :: term
   def min( _root = %BPQHeap{size: 0} ), do: nil
   def min( root ) do
     Enum.min_by( root.forrest, &(&1.score) )
   end
 
+  @doc """
+  Removes the minimum element of the queue structure identified by _root_
+  and returns a new heap structure with the element removed. 
+
+  If the queue is already empty, _Enum.EmptyError_ is raised.
+  """
+  @struct pop( struct ) :: struct
   def pop( _root = %BPQHeap{size: 0} ), do: raise Enum.EmptyError
   def pop( root ) do
     min_node = min( root )
@@ -41,8 +68,17 @@ defmodule BinomialPriorityQueue.Heap do
     new( root.size - 1, new_forrest )
   end
 
+  @doc """
+  Returns the number of elements within the queue identified by _root_.
+  """
+  @struct size( struct ) :: non_neg_integer
   def size( root ), do: root.size
 
+  @doc """
+  Serializes the queue identified by _root_ into a list ordered by the 
+  values scores.
+  """
+  @spec to_list( struct ) :: list
   def to_list( root ), do: to_list( root, [] )
 
   defp to_list( %BPQHeap{ size: 0 }, acc ), do: Enum.reverse( acc )
